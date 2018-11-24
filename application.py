15a6c6ed992c497f28a9e9e1e055225fd02dfe3b
jnk <popokatapepel@xxx.com>
2018-11-24 12:24:55 +0100

    photo screen function worken

--- application.py
+++ application.py
@@ -8,10 +8,16 @@ from json import dumps
 logging.basicConfig(level=logging.DEBUG)
 
 app=Flask(__name__)
+app.config['UPLOAD_FOLDER']='vault'
 
-@app.route("/")
+@app.route("/", methods=['GET','POST'])
 def index():
-    return render_template('index.html')
+    if request.method=='GET':
+        return render_template('photo.html')
+    elif request.method=='POST':
+        file=request.files['image']
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+        return 'this is a post request'
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', threaded=True)
